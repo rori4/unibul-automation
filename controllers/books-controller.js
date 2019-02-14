ced917de2b936a8edd35a64ee2c5bdb3c7c3c4c2
rori4 <RangelStoilov@students.softuni.bg>
2019-02-15 00:35:40 +0200

    added websites and selenium

--- controllers/books-controller.js
+++ controllers/books-controller.js
@@ -1,5 +1,29 @@
+const Book = require("../models/Books/Book");
+const submissions = require('../core/submissions');
 module.exports = {
-    submitGet: (req, res) => {
-        res.render('books/submit');
-    },
+  addGet: (req, res) => {
+    res.render("books/add");
+  },
+  addPost: async (req, res) => {
+    try {
+      let book = req.body;
+      let bookAdded = await Book.create(book);
+      res.redirect('/books/list');
+    } catch (error) {
+        console.log(error);
+    }
+  },
+  listGet: async (req, res) => {
+    try {
+      let books = await Book.find();
+      res.render("books/list", { books });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  listPost: (req, res) => {},
+  submitTest: async (req,res) =>{
+    let book = await Book.findOne();
+    submissions.books(book);
+  }
 };
